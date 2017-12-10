a0c7a7dc619fb39397c7103b1e646d96c4ed7ba3
Nicolás Tamargo <reosarevok@gmail.com>
2017-12-11 00:28:06 +0200

    Update this to use the API. That also means it can now work when there are multiple heritage designations

--- muinas-data.py
+++ muinas-data.py
@@ -1,5 +1,5 @@
 from lxml import html
-import requests
+import urllib, json
 import pywikibot
 from pywikibot import pagegenerators as pg
 
@@ -21,7 +21,7 @@ site = pywikibot.Site("wikidata", "wikidata")
 repo = site.data_repository()
 
 # We get all items that have a muinas.ee ID
-query = "SELECT ?item WHERE { ?item wdt:P2948 [] }"
+query = "SELECT ?item WHERE { ?item wdt:P2948 [] . MINUS {?item wdt:P1435 []} }"
 generator = pg.WikidataSPARQLPageGenerator(query, site=site)
 
 for item in generator:
@@ -30,35 +30,43 @@ for item in generator:
     # There should only ever exist one ID per item, so we take the first
     muinasID = item.claims[u'P2948'][0].target
 
+
     # We scrape the muinas.ee page to get the type of monument and the date of registration
-    page = requests.get("http://register.muinas.ee/public.php?menuID=monument&action=view&id=" + muinasID)
-    tree = html.fromstring(page.content)
-    muinasType = tree.xpath("//th[contains(.,'Liigitus')]/following-sibling::td/text()")
-    muinasDate = tree.xpath("(//th[contains(.,'Registreeritud')])[2]/following-sibling::td/strong/text()")
+    response = urllib.urlopen("https://register.muinas.ee/rest/v1/monuments/" + muinasID)
+    data = json.loads(response.read())
+    muinasTypes = data['classifications']
+    muinasDate = data['registered']
 
     # We turn the scraped monument type into its equivalent Wikidata item
-    muinasTypeItem = muinastype_to_item(muinasType[0])
+    muinasTypeItems = []
+    for type in muinasTypes:
+        muinasTypeItems.append(muinastype_to_item(type))
+
+    print muinasTypeItems
 
     # We turn the scraped date into something Wikidata can recognize
-    tempDate = muinasDate[0].split('.')
-    muinasYear = int(tempDate[2])
+    tempDate = muinasDate.split('-')
+    muinasYear = int(tempDate[0])
     muinasMonth = int(tempDate[1])
-    muinasDay = int(tempDate[0])
+    muinasDay = int(tempDate[2])
     muinasWbDate = pywikibot.WbTime(year=muinasYear, month=muinasMonth, day=muinasDay)
 
     # As long as the type we found matched one of our items, we send the info, including the date, to Wikidata
-    if muinasTypeItem != "nothing" and not (u'P1435' in item.claims):
-        claim = pywikibot.Claim(repo, "P1435")
-        target = pywikibot.ItemPage(repo, muinasTypeItem)
-        claim.setTarget(target)
-        item.addClaim(claim,
-                      summary=u'Importing heritage information from the Estonian National Registry of Cultural Monuments')
-        # Dates currently broken, WIP
-        qualifier = pywikibot.Claim(repo, "P580")
-        qualifier.setTarget(muinasWbDate)
-        claim.addQualifier(qualifier, summary=u'Importing heritage information from the Estonian National Registry of Cultural Monuments')
-        statedin = pywikibot.Claim(repo, "P248")
-        enrcm = pywikibot.ItemPage(repo, "Q3743725")
-        statedin.setTarget(enrcm)
-        claim.addSources([statedin],
-                         summary=u'Importing heritage information from the Estonian National Registry of Cultural Monuments')
+    if muinasTypeItems and not (u'P1435' in item.claims):
+        print "Type!"
+        for type in muinasTypeItems:
+            claim = pywikibot.Claim(repo, "P1435")
+            target = pywikibot.ItemPage(repo, type)
+            claim.setTarget(target)
+            item.addClaim(claim,
+                          summary=u'Importing heritage information from the Estonian National Registry of Cultural Monuments')
+            qualifier = pywikibot.Claim(repo, "P580")
+            qualifier.setTarget(muinasWbDate)
+            claim.addQualifier(qualifier, summary=u'Importing heritage information from the Estonian National Registry of Cultural Monuments')
+            statedin = pywikibot.Claim(repo, "P248")
+            enrcm = pywikibot.ItemPage(repo, "Q3743725")
+            statedin.setTarget(enrcm)
+            claim.addSources([statedin],
+                             summary=u'Importing heritage information from the Estonian National Registry of Cultural Monuments')
+    else:
+        print "No muinastype detected"
