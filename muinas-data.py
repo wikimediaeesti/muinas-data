63ca378124a1dca593395dfb8459bfef6e468668
Nicolás Tamargo <reosarevok@gmail.com>
2017-12-21 11:58:02 +0200

    Remove claims that we scrape stuff since we use the API now

--- muinas-data.py
+++ muinas-data.py
@@ -1,10 +1,9 @@
-from lxml import html
 import urllib, json
 import pywikibot
 from pywikibot import pagegenerators as pg
 
 
-# Turns a scraped muinas.ee monument type into its equivalent Wikidata item
+# Turns a muinas.ee monument type into its equivalent Wikidata item
 def muinastype_to_item(argument):
     switcher = {
         u'ehitism\xe4lestis': "Q12361770",
@@ -20,7 +19,7 @@ def muinastype_to_item(argument):
 site = pywikibot.Site("wikidata", "wikidata")
 repo = site.data_repository()
 
-# We get all items that have a muinas.ee ID
+# We get all items that have a muinas.ee ID and no monument type yet
 query = "SELECT ?item WHERE { ?item wdt:P2948 [] . MINUS {?item wdt:P1435 []} }"
 generator = pg.WikidataSPARQLPageGenerator(query, site=site)
 
@@ -31,18 +30,18 @@ for item in generator:
     muinasID = item.claims[u'P2948'][0].target
 
 
-    # We scrape the muinas.ee page to get the type of monument and the date of registration
+    # We look at the muinas.ee API to get the type of monument and the date of registration
     response = urllib.urlopen("https://register.muinas.ee/rest/v1/monuments/" + muinasID)
     data = json.loads(response.read())
     muinasTypes = data['classifications']
     muinasDate = data['registered']
 
-    # We turn the scraped monument type into its equivalent Wikidata item
+    # We turn the monument type into its equivalent Wikidata item
     muinasTypeItems = []
     for type in muinasTypes:
         muinasTypeItems.append(muinastype_to_item(type))
 
-    # We turn the scraped date into something Wikidata can recognize
+    # We turn the date into something Wikidata can recognize
     tempDate = muinasDate.split('-')
     muinasYear = int(tempDate[0])
     muinasMonth = int(tempDate[1])
